7097bb156817fdc764ba985aaee12c99edb90c18
kartikay-28 <kartikayrattan1@gmail.com>
2025-06-23 16:50:06 +0530

    Updated backend for search

--- Backend/routes/destinationRoutes.js
+++ Backend/routes/destinationRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getDestinations, addDestination } = require('../controllers/destinationController.js');
+const { getDestinations, addDestination, getDestinationByName } = require('../controllers/destinationController.js');
 
 const router = express.Router();
 
 router.get('/', getDestinations);
 router.post('/add', addDestination);
+router.get('/:placename', getDestinationByName);
 
 module.exports = router;
